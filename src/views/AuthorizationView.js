abfb0d2c02b6640f33426d583a23b0b24bc9f5c4
Alexandr <70664414+alex-voytko@users.noreply.github.com>
2021-10-06 03:35:44 +0300

    1.0.1

---
+++ src/views/AuthorizationView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AppBar from "../components/AppBar";
+import AuthForm from "../components/AuthForm";
+
+const AuthorizationView = () => {
+  return (
+    <>
+      <AppBar />
+      <div></div>
+    </>
+  );
+};
+
+export default AuthorizationView;
