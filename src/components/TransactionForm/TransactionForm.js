79f17685a93898b805b8f31d73d967c83d781561
Inna Kovtunenko <kovshikinna@gmail.com>
2021-10-08 17:26:46 +0300

    form for TransactionForm

--- src/components/TransactionForm/TransactionForm.js
+++ src/components/TransactionForm/TransactionForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Calendar from '../Calendar';
 
+
 import s from './TransactionForm.module.scss';
 
 const TransactionForm = () => {
@@ -12,7 +13,20 @@ const TransactionForm = () => {
 selectedDate={startDate} onChange={(date) => setStartDate(date)} />
             <div className={s.inputTransaction}>
                 <input type="text" placeholder="Описание товара"/>
-                <input type="text" placeholder="Категория товара"/>
+<select >
+    <option selected>Категория товара</option>
+    <option value="transport">Транспорт</option>
+    <option value="products">Продукты</option>
+    <option value="health">Здоровье</option>
+    <option value="alcohol">Алкоголь</option>
+    <option value="entertainment">Развлечения</option>
+    <option value="housing">Всё для дома</option>
+    <option value="technique">Техника</option>
+    <option value="communication">Коммуналка, связь</option>
+    <option value="hobbies">Спорт, хобби</option>
+    <option value="education">Образование</option>
+    <option value="other">Прочее</option>
+    </select>                
                 <input type="text" placeholder="0,00"/>
             </div>
             <div>
