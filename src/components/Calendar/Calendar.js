12ec7e00518e8ddf208a631e3fd017f152c0a596
Inna Kovtunenko <kovshikinna@gmail.com>
2021-10-08 18:31:16 +0300

    add TransactionForm

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import s from './Calendar.module.scss';
+
+
+const Calendar = ({selectedDate, handleChange}) => (
+    <div className={s.calendarContainer}>  
+        <label className={s.calendarIcon} htmlForm="calendar"/> 
+
+            <DatePicker className={s.datepicker}
+             id="datepicker" 
+             selected={selectedDate}
+              onChange={handleChange}
+               dateFormat="dd.MM.yyyy"
+                name="date" 
+                fixedHeight />
+        </div>
+)
+
+export default Calendar;
