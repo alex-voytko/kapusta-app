d4fa90e941f607bc06b7a187345688d8924c6bca
Alexandr <70664414+alex-voytko@users.noreply.github.com>
2021-10-11 21:11:10 +0300

    in process 1.4.0

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import AppBar from './components/AppBar';
-import { Suspense, lazy } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
-const AuthorizationView= lazy(() =>
-  import("./views/AuthorizationView")
-);
+const AuthorizationView = lazy(() => import('./views/AuthorizationView'));
 
-const HomeView = lazy(() =>
-  import("./views/HomeView")
-);
-const ReportsView = lazy(() =>
-  import("./views/ReportsView")
-);
-function App () {
+const HomeView = lazy(() => import('./views/HomeView'));
+const ReportsView = lazy(() => import('./views/ReportsView'));
+function App() {
     return (
         <div>
+            <AppBar />
             <Suspense fallback={<p>...Loading</p>}>
                 <Switch>
                     <Route exact path="/Home">
@@ -28,12 +23,9 @@ function App () {
                     <Route path="/Reports">
                         <ReportsView />
                     </Route>
-                {/* <Redirect to ="/Home">
-             <HomeView />
-             </Redirect> */}
                 </Switch>
             </Suspense>
         </div>
-    )
+    );
 }
 export default App;
