eaff4959082745ed854fb1b1d7c1631601297826
Inna Kovtunenko <kovshikinna@gmail.com>
2021-10-13 11:31:47 +0300

    add form TransactionList

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -3,7 +3,10 @@ import TransactionForm from '../../components/TransactionForm';
 import Balance from '../../components/Balance/Balance';
 import Summary from '../../components/Summary/Summary';
 import Container from '../../components/Container';
+import TransactionList from '../../components/TransactionList';
 // import ModalZeroBalance from '../../components/Modal/ModalZeroBalance'
+
+import s from './HomeView.module.scss'
 
 function HomeView() {
     return (
@@ -11,8 +14,13 @@ function HomeView() {
             <Container>
                 <Balance />
                 {/* <ModalZeroBalance /> */}
-                <TransactionForm />
-                <Summary />
+                <div className={s.container}>
+                    <TransactionForm />
+                    <div className={s.accounting}>
+                      <TransactionList />
+                <Summary />  
+                    </div>                
+                </div>          
             </Container>
         </>
     );
