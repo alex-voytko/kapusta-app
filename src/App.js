971d85c95967f4a1da758ee21fd523cc3cc5074b
Maksym Chervinskyi <66177888+Gaivir@users.noreply.github.com>
2021-10-16 17:46:22 +0300

    Блок Reports

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
                     <Route path="/Authorization">
                         <AuthorizationView />
                     </Route>
-                    <Route path="/Reports">
+                    <Route path="/reports">
                         <ReportsView />
                     </Route>
                 </Switch>
--- src/views/ReportsView/ReportsView.js
+++ src/views/ReportsView/ReportsView.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import SwiperReport from '../../components/Swiper/SwiperReport';
+import ReportsStatistics from '../../components/ReportsStatistics/ReportsStatistics';
+import Chartjs from '../../components/Chartjs/Chartjs';
+import styles from './Report.module.css';
 
-const ReportsView = () => {
-    return <></>;
-};
+function ReportsView() {
+    return (
+        <>
+            <div className={styles.categories}>
+                <div className={styles.dataline}>
+                    <div className={styles.dataline_block}>
+                        <span className={styles.dataline_label}>Расходы:</span>
+                        <span className={styles.dataline_expenses}>
+                            -18 000.00
+                        </span>
+                    </div>
+                    <span className={styles.datalineJumper} />
+                    <div className={styles.dataline_block}>
+                        <span className={styles.dataline_label}>Доходы:</span>
+                        <span className={styles.dataline_income}>
+                            +45 000.00
+                        </span>
+                    </div>
+                </div>
+                <div className={styles.expencesBlock}>
+                    <SwiperReport />
+                    <ReportsStatistics />
+                </div>
+                <div className={styles.expencesBlock}>
+                    <div className={styles.chartBlock}>
+                        <Chartjs />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
 
 export default ReportsView;
